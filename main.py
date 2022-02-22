876cd10aeba09a26315b28aa33eb5f6a2174b4b0
Ankrutee <79003737+Ankrutee@users.noreply.github.com>
2022-02-22 23:52:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import akinator
+aki = akinator.Akinator()
+q = aki.start_game()
+while aki.progression <= 80:
+    a = input(q +"\n\t")
+    if a == 'b':
+        try:
+            q = aki.back()
+        except akinator.CantGoBackAnyFurhter:
+            pass
+    else:
+        q = aki.answer(a)
+aki.win()
+correct = input(f"Its {aki.first_guess['name']} "
+                f""
+                f"({aki.first_guess['description']})! Was i correct?\n{aki.first_guess['absolute_picture_path']}\n\t")
+if correct.lower() == 'yes' or correct.lower() == 'y':
+    print("yes i got it\n")
+else:
+    print("ohh just missed\n")
